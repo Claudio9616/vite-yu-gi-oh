39b1a129a21dbd85033340034c1e361f764e6ff4
Claudio9616 <clares9611@gmail.com>
2024-02-05 17:57:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+import { store } from './store';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+export default {
+  name: 'yu-gi-oh',
+  data: () => ({
+    pokemons: []
+  }),
+  created() {
+    axios.get(endpoint).then(res => {
+      this.pokemons = res.data.docs
+    })
+    console.log(endpoint)
+  }
+
+}
+</script>
+<template>
+  <h1 class="container text-danger">Ciao</h1>
+</template>
+<style lang="scss">
+@use './assets/stylesass/style.scss';
+</style>
